562450b97cd49d32574e3f49b8be5d5f2ba013db
Ghislaine Laios <laios54@proton.me>
2022-11-27 15:53:11 +0800

    Complete the homework of 09-26.

---
+++ Project0926/MainModelView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project0926
+{
+    class MainModelView: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        public ObservableCollection<FileSystemInfo> Files { get; } = new ObservableCollection<FileSystemInfo>();
+    }
+}
---
+++ Project0926/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project0926
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private TreeViewItem _dummyItem = new TreeViewItem();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (string s in Directory.GetLogicalDrives())
+            {
+                AddItemToTree(FolderTree, s, s);
+            }
+        }
+
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            var root = (TreeViewItem)sender;
+            if (root.Items.Count > 0) return;
+            root.Items.Clear();
+            var path = root.Tag.ToString()!;
+            UpdateFiles(path);
+            foreach (string s in Directory.GetDirectories(path))
+            {
+                AddItemToTree(root, s, s.Substring(s.LastIndexOf("\\", StringComparison.Ordinal) + 1));
+            }
+        }
+
+        private void AddItemToTree(ItemsControl root, string path, string header)
+        {
+            var item = new TreeViewItem
+            {
+                Header = header,
+                Tag = path
+            };
+            item.Expanded += Folder_Expanded;
+            root.Items.Add(item);
+        }
+
+        private void UpdateFiles(string dirPath)
+        {
+            ModelView.Files.Clear();
+            var info = new DirectoryInfo(dirPath);
+            foreach (var directoryInfo in info.GetDirectories())
+            {
+                ModelView.Files.Add(directoryInfo);
+            }
+
+            foreach (var fileInfo in info.GetFiles())
+            {
+                ModelView.Files.Add(fileInfo);
+            }
+        }
+
+        private MainModelView ModelView => (MainModelView)this.DataContext;
+
+        private void ListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var listView = (ListView) sender;
+            var selected = listView.SelectedItem;
+            if (selected is null) return;
+            var info = (FileSystemInfo) selected;
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "explorer",
+                Arguments = "\"" + info.FullName + "\""
+            });
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
